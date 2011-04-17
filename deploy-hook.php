d428fadcf1dd9bb2bfa5659f2c1276af96f5aeae
Ming-Ting Yao Wei <mwei@lxde.org>
2011-04-17 20:01:27 +0800

    fix svn revert used by deploy-hook.php

--- deploy-hook.php
+++ deploy-hook.php
@@ -3,7 +3,7 @@ include_once("config.php");
 include_once("deploy.php");
 
 print ("= Updating Deploy Script =\n");
-system ("svn revert -R .");
+system ("svn revert *");
 system ("svn update");
 system ("svn info");
 print ("\n");
