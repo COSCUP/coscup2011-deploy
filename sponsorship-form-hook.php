ce7a3e5ee58822b1c07ec24ee07908b3ac499ea6
Xaver Y.R. Chen <yrchen@ATCity.org>
2011-04-17 19:19:50 +0800

    Add sync sponsorchip form module to CMS to sponsorship-form-hook

---
+++ sponsorship-form-hook.php
@@ -0,0 +1,14 @@
+<?php
+include_once("deploy.php");
+
+print ("= Updating Sponsorship Form =\n");
+system ("svn update " . SPONSORSHIP_FORM_PATH);
+system ("svn info " . SPONSORSHIP_FORM_PATH);
+print ("\n");
+
+print ("= Syncing Sponsorship Form =\n");
+system ('rsync -av --delete ' . SPONSORSHIP_FORM_PATH.'drupal/' . ' ' . CMS_MODULE_PATH . ' 2>&1');
+print ("\n");
+
+recompile_and_sync();
+?>
