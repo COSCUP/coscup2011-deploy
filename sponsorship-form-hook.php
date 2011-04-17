923b450b44128d9dab01f5ecaaa903c6dede0a56
Ming-Ting Yao Wei <mwei@lxde.org>
2011-04-17 19:57:27 +0800

    fix sponsorship-form-hook path

--- sponsorship-form-hook.php
+++ sponsorship-form-hook.php
@@ -7,6 +7,6 @@ system ("svn info " . SPONSORSHIP_FORM_PATH);
 print ("\n");
 
 print ("= Syncing Sponsorship Form =\n");
-system ('rsync -av --delete ' . SPONSORSHIP_FORM_PATH.'drupal/' . ' ' . CMS_MODULE_PATH . ' 2>&1');
+system ('rsync -av --delete ' . SPONSORSHIP_FORM_PATH . ' ' . CMS_MODULE_PATH . ' 2>&1');
 print ("\n");
 ?>
