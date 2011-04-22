76e835225160ea40dc68916bfd3e000bcbb62a68
Ming-Ting Yao Wei <mwei@lxde.org>
2011-04-22 21:29:43 +0800

    add shiny red button. closes #4.

--- index.php
+++
@@ -1,2 +0,0 @@
-<?php
-/* TODO: a button to update gdoc */
--- update-gdoc.php
+++ update-gdoc.php
@@ -5,6 +5,40 @@ print ("= Reverting Source SVN =\n");
 system ("svn revert -R " . SRC_PATH);
 print ("\n");
 
+if ($_POST["fullupdate"] == 1)
+{
+	print ("= Updating Marksite =\n");
+	system ("svn update " . MARKSITE_PATH);
+	system ("svn info " . MARKSITE_PATH);
+	print ("\n");
+
+
+	print ("= Updating Sponsorship Form =\n");
+	system ("svn update " . SPONSORSHIP_FORM_PATH);
+	system ("svn info " . SPONSORSHIP_FORM_PATH);
+	print ("\n");
+
+	print ("= Syncing Sponsorship Form =\n");
+	system ('rsync -av --delete ' . SPONSORSHIP_FORM_PATH . ' ' . CMS_MODULE_PATH . ' 2>&1');
+	print ("\n");
+
+
+	print ("= Updating Source SVN =\n");
+	system ("svn update " . SRC_PATH);
+	system ("svn info " . SRC_PATH);
+	print ("\n");
+
+	print ("= Updating Theme =\n");
+	system ("svn revert -R " . THEME_PATH);
+	system ("svn update " . THEME_PATH);
+	system ("svn info " . THEME_PATH);
+	print ("\n");
+
+	print ("= Syncing Theme =\n");
+	system ('rsync -av --delete ' . THEME_PATH.'drupal/' . ' ' . CMS_THEME_PATH . ' 2>&1');
+	print ("\n");
+}
+
 print ("= Updating GDoc =\n");
 include ("update-gdoc-functions.php");
 print ("\n");
