d8c7768207080fc42dce3ae2a5bf580ef042588c
Ming-Ting Yao Wei <medicalwei@gmail.com>
2011-05-05 01:29:24 +0800

    use "git" branch

--- deploy-hook.php
+++ deploy-hook.php
@@ -4,5 +4,5 @@ include_once("deploy.php");
 
 print ("= Updating Deploy Script =\n");
 system ("git reset --hard");
-system ("git pull origin master");
+system ("git pull origin git");
 print ("\n");
