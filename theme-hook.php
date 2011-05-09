fe7f508576e8e0ad97636ff8dffac84b974b1942
Ming-Ting Yao Wei <medicalwei@gmail.com>
2011-05-09 14:08:13 +0800

    removing unused git_cwd

--- theme-hook.php
+++ theme-hook.php
@@ -3,8 +3,8 @@ include_once("deploy.php");
 
 print ("= Updating Theme =\n");
 chdir (THEME_PATH);
-system ("git " . git_cwd(THEME_PATH) . " reset --hard");
-system ("git " . git_cwd(THEME_PATH) . " pull origin master");
+system ("git reset --hard");
+system ("git pull origin master");
 system ("git log -1");
 chdir ($cwd);
 print ("\n");
